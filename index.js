edca66b8f9a22b0ab6b480e95e007d6753ee6081
ewnd9 <ewndnine@gmail.com>
2015-06-20 00:05:39 +0300

    dialog with torrent tracker select

--- index.js
+++ index.js
@@ -1,99 +1,11 @@
 #!/usr/bin/env node
 
-var config = require('dot-file-config')('.pw3-npm', __dirname + '/default-config.json');
-
-var _ = require('lodash');
-var tpb = require('thepiratebay');
-var prompt = require('prompt');
-var Table = require('cli-table');
-var sys = require('sys');
-var exec = require('child_process').exec;
-var Q = require('q');
-
 var argv = require('minimist')(process.argv.slice(2));
-var table = new Table({
-  head: ['#', 'Name', 'Size', 'Stats']
-});
 
-var result = [];
-
-var ioLoop = function(hideTable) {
-  if (!hideTable) {
-    console.log(table.toString());
-  }
-
-  process.stdout.write('select: ');
-  process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-};
-
-process.stdin.on('data', function (text) {
-  var index = parseInt(text);
-  var r = result[index - 1];
-
-  var command = config.data.exec.replace('$torrent', r.magnet);
-
-  console.log('running "' + r.name + '":\n' + command);
-  exec(command);
-
-  ioLoop(true);
-});
-
-var filterBySubstrings = function(results, substrings) {
-  return _.filter(results, function(val) {
-    var item = _.find(substrings, function(substring) {
-      return val.name.indexOf(substring) === -1;
-    });
-
-    return typeof item === 'undefined';
-  });
-};
-
-var adapters = {
-  tpb: require('./src/tpb-adapter'),
-  kickass: require('./src/kickass-adapter')
-}
-
-var search = function(query) {
-  return adapters[argv['adapter'] || 'tpb'].query(query).then(function(results) {
-    if (argv['c']) {
-      results = filterBySubstrings(results, argv['c'].split(','));
-    }
-
-    return results;
-  });
-};
-
-var query = argv['_'].join(' ');
-var regex = /[eE]([\d]+)\-([\d]+)/;
-var found = query.match(regex);
-var xs = [];
-
-var tr = function(s, n) {
-  return (s.length >= n) ? s : tr('0' + s, n);
-}
-
-if (found) {
-  xs = _.map(_.range(parseInt(found[1]), parseInt(found[2]) + 1), function(i) {
-    return search(query.replace(found[0], 'e' + tr(i + '', 2)));
-  });
+if (argv.setup === true) {
+  require('./lib/setup-task.js')();
+} else if (argv.adapter) {
+  require('./lib/search-task.js').search(argv.adapter);
 } else {
-  xs = [search(query)];
+  require('./lib/search-task.js').withDialog();
 }
-
-Q.all(xs).then(function(results) {
-  result = _.flatten(results);
-
-  if (result.length === 0) {
-    console.log('0 results');
-    process.exit();
-  }
-
-  _.each(result, function(val, i) {
-    table.push(
-      [(i + 1) + '', val.name, val.size, val.seeders + '/' + val.leechers]
-    );
-  });
-
-  ioLoop();
-});
